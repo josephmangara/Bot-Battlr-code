cf4472f826bb674b7d40f67b89131268b8dca958
josephmangara <josephmangara2@gmail.com>
2023-11-19 18:52:16 +0300

    added json data and fetched it

--- bot-battlr/src/App.js
+++ bot-battlr/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState, useEffect } from 'react';
 function App() {
+
+const [bots, setBots] = useState([])
+const apiURL = "http://localhost:3000/bots"
+useEffect(() => {
+  fetch(apiURL)
+  .then(res => res.json())
+  .then(data => {
+    console.log(data)
+    setBots(data)
+  })
+}, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   <>
+   </>
+  )
 }
 
 export default App;
+
